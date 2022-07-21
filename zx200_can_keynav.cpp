889567ac850bdf638e32f213f0996cab1abedb66
Genki Yamauchi <yamauchi-g573bs@pwri.go.jp>
2022-07-21 14:01:51 +0300

    change struct name and add two cmd struct

--- zx200_can_keynav.cpp
+++ zx200_can_keynav.cpp
@@ -27,7 +27,7 @@ struct frame
   std::array<std::uint8_t, 8> payload;
 };
 
-struct Lever_cmd
+struct pilot_pressure_cmd1
 {
   std::uint8_t boom_raise;
   std::uint8_t boom_lower;
@@ -39,6 +39,29 @@ struct Lever_cmd
   std::uint8_t swing_right;
 };
 
+struct pilot_pressure_cmd2
+{
+  std::uint8_t track_right_forward;
+  std::uint8_t track_left_forward;
+  std::uint8_t track_right_backward;
+  std::uint8_t track_left_backward;
+  std::uint8_t spare_A;
+  std::uint8_t spare_B;
+  std::uint8_t assist_A;
+  std::uint8_t assist_B;
+};
+
+struct machine_setting_cmd
+{
+  std::uint8_t engine_rpm;
+  std::uint8_t power_eco_mode;
+  std::uint8_t rabbit_turtle_mode;
+  std::uint8_t status_notice;
+  std::uint8_t yellow_lamp_cmd;
+  // byte3-7 is not assigned
+  // std::uint8_t alive_cnt;
+};
+
 class can_handler
 {
   public:
@@ -46,82 +69,90 @@ class can_handler
         : send_timer(io, boost::asio::chrono::milliseconds(cmd_interval)),
           send_timer1(io, boost::asio::chrono::milliseconds(cmd1_interval)),
           send_timer2(io, boost::asio::chrono::milliseconds(cmd2_interval)),
-          sock(io)
+          sock(io), alive_cnt(0)
     {
       const auto idx = canary::get_interface_index(can_port);
       auto const ep = canary::raw::endpoint{idx};
       sock.open();
       sock.bind(ep);
 
-      // l_cmd = Lever_cmd{};
-      l_cmd2 = boost::shared_ptr<Lever_cmd>(new Lever_cmd{});
+      pi_cmd1 = boost::shared_ptr<pilot_pressure_cmd1>(new pilot_pressure_cmd1{});
+      pi_cmd2 = boost::shared_ptr<pilot_pressure_cmd2>(new pilot_pressure_cmd2{});
+      setting_cmd = boost::shared_ptr<machine_setting_cmd>(new machine_setting_cmd{});
 
-      std::cout << "shared_ptr:" << l_cmd2->swing_right << std::endl;
-
-      // l_cmd[0].
       start_receive();
-      send_timer.async_wait(boost::bind(&can_handler::send_cmd, this));
-      send_timer1.async_wait(boost::bind(&can_handler::send_cmd1, this));
-      send_timer2.async_wait(boost::bind(&can_handler::send_cmd2, this));
+      send_timer.async_wait(boost::bind(&can_handler::send_pi_cmd1, this));
+      send_timer1.async_wait(boost::bind(&can_handler::send_pi_cmd2, this));
+      send_timer2.async_wait(boost::bind(&can_handler::send_machine_setting_cmd, this));
     }
-    void send_cmd()
+
+    void send_pi_cmd1()
     {
         std::cout << "Test" << std::endl;
 
         frame f = {};
-        f.header.id(0x12345678);
+        f.header.id(0x18FF640A);
         f.header.extended_format(true);
         f.header.payload_length(8);
-        f.payload[0] = l_cmd2->boom_raise;
-        f.payload[1] = l_cmd2->boom_lower;
-        f.payload[2] = l_cmd2->arm_dump;
-        f.payload[3] = l_cmd2->arm_crowd;
-        f.payload[4] = l_cmd2->bucket_dump;
-        f.payload[5] = l_cmd2->bucket_crowd;
-        f.payload[6] = l_cmd2->swing_left;
-        f.payload[7] = l_cmd2->swing_right;
+        f.payload[0] = pi_cmd1->boom_raise;
+        f.payload[1] = pi_cmd1->boom_lower;
+        f.payload[2] = pi_cmd1->arm_dump;
+        f.payload[3] = pi_cmd1->arm_crowd;
+        f.payload[4] = pi_cmd1->bucket_dump;
+        f.payload[5] = pi_cmd1->bucket_crowd;
+        f.payload[6] = pi_cmd1->swing_left;
+        f.payload[7] = pi_cmd1->swing_right;
 
         // sock.send(canary::net::buffer(&f, sizeof(f)));
         sock.async_send(canary::net::buffer(&f, sizeof(f)), boost::bind(&can_handler::nullhandle,this));
         send_timer.expires_at(send_timer.expiry() + boost::asio::chrono::milliseconds(cmd_interval));
-        send_timer.async_wait(boost::bind(&can_handler::send_cmd, this));
+        send_timer.async_wait(boost::bind(&can_handler::send_pi_cmd1, this));
     }
-    void send_cmd1()
+
+    void send_pi_cmd2()
     {
       std::cout << "Test1" << std::endl;
 
       frame f = {};
-      f.header.id(0x1234567A);
+      f.header.id(0x18FF650A);
       f.header.extended_format(true);
       f.header.payload_length(8);
+      f.payload[0] = pi_cmd2->track_right_forward;
+      f.payload[1] = pi_cmd2->track_left_forward;
+      f.payload[2] = pi_cmd2->track_right_backward;
+      f.payload[3] = pi_cmd2->track_left_backward;
+      f.payload[4] = pi_cmd2->spare_A;
+      f.payload[5] = pi_cmd2->spare_B;
+      f.payload[6] = pi_cmd2->assist_A;
+      f.payload[7] = pi_cmd2->assist_B;
 
       // sock.send(canary::net::buffer(&f, sizeof(f)));
       sock.async_send(canary::net::buffer(&f, sizeof(f)), boost::bind(&can_handler::nullhandle, this));
       send_timer1.expires_at(send_timer1.expiry() + boost::asio::chrono::milliseconds(cmd1_interval));
-      send_timer1.async_wait(boost::bind(&can_handler::send_cmd1, this));
+      send_timer1.async_wait(boost::bind(&can_handler::send_pi_cmd2, this));
     }
-    void send_cmd2()
+
+    void send_machine_setting_cmd()
     {
       std::cout << "Test2" << std::endl;
-      
-      ///
-      //boost share_ptr入れる？？？
-      ///
 
       frame f = {};
-      f.header.id(0x1234567B);
+      f.header.id(0x18FF660A);
       f.header.extended_format(true);
       f.header.payload_length(8);
+      f.payload[0] = setting_cmd->engine_rpm;
+      f.payload[1] = std::uint8_t(
+          setting_cmd->yellow_lamp_cmd << 4 | setting_cmd->status_notice << 3 |setting_cmd->rabbit_turtle_mode << 2 | setting_cmd->power_eco_mode);
+      f.payload[7] = alive_cnt++;
 
       // sock.send(canary::net::buffer(&f, sizeof(f)));
       sock.async_send(canary::net::buffer(&f, sizeof(f)), boost::bind(&can_handler::nullhandle, this));
       send_timer2.expires_at(send_timer2.expiry() + boost::asio::chrono::milliseconds(cmd2_interval));
-      send_timer2.async_wait(boost::bind(&can_handler::send_cmd2, this));
+      send_timer2.async_wait(boost::bind(&can_handler::send_machine_setting_cmd, this));
     }
-    void lever_cmd(Lever_cmd cmd)
+    void set_pilot_pressure_cmd1(pilot_pressure_cmd1 cmd)
     {
-      //l_cmd = cmd;
-      l_cmd2 = boost::make_shared<Lever_cmd>(cmd);
+      pi_cmd1 = boost::make_shared<pilot_pressure_cmd1>(cmd);
     }
 
     private:
@@ -146,24 +177,22 @@ class can_handler
       }
       boost::asio::steady_timer send_timer, send_timer1, send_timer2;
       canary::raw::socket sock;
-      // Lever_cmd l_cmd;
-      boost::shared_ptr<Lever_cmd> l_cmd2;
-    };
+      boost::shared_ptr<pilot_pressure_cmd1> pi_cmd1;
+      boost::shared_ptr<pilot_pressure_cmd2> pi_cmd2;
+      boost::shared_ptr<machine_setting_cmd> setting_cmd;
+      std::uint8_t alive_cnt;
+};
+
+
 
 int main(int argc, char **argv)
 {
-  //io setting
   canary::net::io_context ioc;
-
   can_handler h(ioc,"vcan0");
 
-  // ioc.run();
   boost::thread t(boost::bind(&boost::asio::io_context::run, &ioc));
   while(true){
     sleep(1);
-    // Lever_cmd cmd = Lever_cmd{};
-    // cmd.boom_raise = 100;
-    // h.lever_cmd(cmd);
     std::cout << "main loop" << std::endl;
   }
 }
